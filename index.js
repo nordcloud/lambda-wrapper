bed6f9a6e25bd3b98cc096d01917eb4764d1ac9c
Mikael Puittinen <mikael@iki.fi>
2015-07-23 12:18:19 +0300

    Verify code style using jscs / jshint

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
 // Pseudowrapper for AWS Lambda
-// 
+
 var lambdaModule;
 
-var _runner = function(event, callbackFn) {	
-	if (!lambdaModule) {
-		return callbackFn('Module not initialized',null);
-	}
+var _runner = function(event, callbackFn) {
+    if (!lambdaModule) {
+        return callbackFn('Module not initialized', null);
+    }
 
-	var lambdacontext = {
-		succeed : function(success) {
-			return callbackFn(null, success);
-		},
-		fail : function(error) {				
-			return callbackFn(error,null);
-		}
-	};
-	try {
-		lambdaModule.handler(event, lambdacontext);
-	} catch (ex) {
-		callbackFn('Exception:' + ex.toString());
-	}
+    var lambdacontext = {
+        succeed: function(success) {
+            return callbackFn(null, success);
+        },
+        fail: function(error) {
+            return callbackFn(error, null);
+        }
+    };
+
+    try {
+        lambdaModule.handler(event, lambdacontext);
+    } catch (ex) {
+        callbackFn('Exception:' + ex.toString());
+    }
 };
 
 // Public interface for the module
 module.exports = exports = {
-	init : function(modName) {
-		if (!modName) {
-			modName = '';
-		}
+    init: function(modName) {
+        if (!modName) {
+            modName = '';
+        }
 
-		try {
-			lambdaModule = require(modName);
-			// Return the module;
-			return this;
-		} catch(ex) {
-			lambdaModule = require(process.cwd() + '/' + modName);
-			return this;
-		} 
-		return null;
-	},
-	run: _runner
+        try {
+            lambdaModule = require(modName);
+            return this;
+        } catch (ex) {
+            lambdaModule = require(process.cwd() + '/' + modName);
+            return this;
+        }
+
+        return null;
+    },
+    run: _runner
 };
