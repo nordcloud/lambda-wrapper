d985a7fc79cbca9f205836206165d823975748d6
YaroslavApatiev <yapatiev@haftahave.com>
2018-04-26 17:21:38 +0300

    Async handler support for nodejs8.10

--- index.js
+++ index.js
@@ -37,7 +37,13 @@ class Wrapped {
 
       try {
         if (this.handler) {
-          this.handler(event, lambdaContext, callback);
+          if (process.version === 'v8.10.0' && !cb) {
+            this.handler(event, lambdaContext).then(function (result) {
+              resolve(result);
+            });
+          } else {
+            this.handler(event, lambdaContext, callback);
+          }
         } else {
           if (this.lambdaModule.region) {
             AWS.config.update({
