7b1c704fa4144a94f05c84a4d5a632fffbc98112
mpuittinen <mikael@iki.fi>
2018-05-08 09:03:36 +0300

    Add tests for async + callback

--- test/async/async-wrapper.js
+++ test/async/async-wrapper.js
@@ -14,6 +14,17 @@ const testMod7 = {
   }
 };
 
+const testMod8 = {
+  handler: async (event, context, callback) => {
+    if (event.test === "success") {
+      callback(null, "Success");
+    }
+    if (event.test === "fail") {
+     callback(new Error("Fail"));
+    }
+  }
+};
+
 describe("lambda wrapper local async", () => {
   it("wrap + run async module 7 - await", async () => {
     const w = wrapper.wrap(testMod7);
@@ -33,6 +44,33 @@ describe("lambda wrapper local async", () => {
 
   it("wrap + run async module 7 - exception", async () => {
     const w = wrapper.wrap(testMod7);
+
+    try {
+      const response = await w.run({ test: "fail" });
+      expect(response).to.be.null;
+    } catch (err) {
+      expect(err).to.be.instanceof(Error);
+    }
+  });
+
+  it("wrap + run async module 8 (callback) - await", async () => {
+    const w = wrapper.wrap(testMod8);
+
+    const response = await w.run({ test: "success" });
+    expect(response).to.be.equal("Success");
+  });
+
+  it("wrap + run async module 8 (callback) - promise", done => {
+    const w = wrapper.wrap(testMod8);
+
+    w.run({ test: "success" }).then(response => {
+        expect(response).to.be.equal("Success");
+        done();
+      }).catch(done);
+  });
+
+  it("wrap + run async module 8 (callback) - exception", async () => {
+    const w = wrapper.wrap(testMod8);
 
     try {
       const response = await w.run({ test: "fail" });
